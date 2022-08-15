3710f2d8989037880d675595c409f06e44d97b72
Daniel Felipe <daniell.felipe.2017@gmail.com>
2022-08-14 23:27:21 -0300

    Installing and configuring Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Hello, {{ name }}!</h2>
+
+        <p>In this repo, we will use the following frameworks:</p>
+        <ul>
+            <li v-for="framework of frameworks" v-text="framework"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+};
+</script>
